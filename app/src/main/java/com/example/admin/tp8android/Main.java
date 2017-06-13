85c24e9aa6c5a6c40bdd577641911cac432e5323
Ronan Merien <ronan.merien@gmail.com>
2017-06-13 12:12:34 +0200

    Direct message management

--- app/src/main/java/com/example/admin/tp8android/Main.java
+++ app/src/main/java/com/example/admin/tp8android/Main.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,12 +16,24 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void send(View view) {
+    //Use only one of the following methode
+
+    //Application message management
+    /*public void send(View view) {
         String numero = ((TextView)findViewById(R.id.numero)).getText().toString();
         String text = ((TextView)findViewById(R.id.text)).getText().toString();
         String uri = "smsto:" + numero;
         Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(uri));
         intent.putExtra("sms_body", text);
         startActivity(intent);
+    }
+    */
+
+    //Direct message management
+    public void send(View view) {
+        String numero = ((TextView)findViewById(R.id.numero)).getText().toString();
+        String text = ((TextView)findViewById(R.id.text)).getText().toString();
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(numero, null, text, null, null);
     }
 }
