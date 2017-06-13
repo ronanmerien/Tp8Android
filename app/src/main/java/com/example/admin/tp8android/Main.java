c34648cc796f61f7d0e288ea67dec0cae3c4e037
Ronan Merien <ronan.merien@gmail.com>
2017-06-13 12:00:08 +0200

    add phone permission

--- app/src/main/java/com/example/admin/tp8android/Main.java
+++ app/src/main/java/com/example/admin/tp8android/Main.java
@@ -1,7 +1,11 @@
 package com.example.admin.tp8android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+        String numero = ((TextView)findViewById(R.id.numero)).getText().toString();
+        String text = ((TextView)findViewById(R.id.text)).getText().toString();
+        String uri = "smsto:" + numero;
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(uri));
+        intent.putExtra("sms_body", text);
+        startActivity(intent);
     }
 }
